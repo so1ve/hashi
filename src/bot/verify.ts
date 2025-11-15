569b941f06d90ea10fa0c7aae96b7f3a7514e00f
so1ve <i@mk1.io>
2025-11-16 00:06:32 +0800

    chore: remove delete message temporarily

--- src/bot/verify.ts
+++ src/bot/verify.ts
@@ -1,14 +1,12 @@
 import * as kv from "../kv";
 import { bot } from ".";
 
-export async function verifySuccess(chatId: number, messageId: string) {
-	console.log({ userId: chatId, messageId });
-
+export async function verifySuccess(chatId: number, _messageId: string) {
 	const user = await kv.users.get(chatId);
 	if (user) {
 		await kv.users.set(chatId, { ...user, verified: true });
 	}
-	await bot.api.deleteMessage(chatId, Number.parseInt(messageId));
+	// await bot.api.deleteMessage(chatId, Number.parseInt(messageId));
 	await bot.api.sendMessage(
 		chatId,
 		"Verification successful! You can now use the bot.",
